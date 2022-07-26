bfa1756e71af49437645152ba9814823c52f8d1a
Alex <friedmanalexg@gmail.com>
2022-07-25 21:51:22 -0400

    darkmode splashed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false)
+  
+  
+  function handleCart(){
+    setInCart(!inCart);
+    
+  }
+
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add to cart" onClick={handleCart} >Add to Cart</button>
     </li>
   );
 }
